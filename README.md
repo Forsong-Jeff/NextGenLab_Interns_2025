e0a33110c129de2bc3dc5ab6e24473407531e368
Forsong-Jeff <forsongjeff25p5a@gmail.com>
2025-07-27 01:12:45 +0100

    Initial project setup: main.cpp stub & README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Calculator Feature Sprint
+
+**Compile:**  
+
+g++ main.cpp *.cpp -o calculator
+
+
+**Run:**  
+
+./calculator
+
+
+*(After your feature is merged, examples will appear here.)*
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Declarations for the 8 functions:
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+int modulus(int a, int b);
+int power(int a, int b);
+int maxValue(int a, int b);
+int minValue(int a, int b);
+
+int main() {
+    std::cout << "Calculator Initialized\n";
+    // each function will be called here once implemented
+    return 0;
+}
