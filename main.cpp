8616432642c3c154f3af5de98d20050de1d25d8f
cheque23 <chequenorvan@gmail.com>
2025-07-28 00:23:15 +0100

    Updatedtask

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+int maxValue(int a, int b){
+    return std::max(a,b);
+}
 // Declarations for the 8 functions:
 int add(int a, int b);
 int subtract(int a, int b);
@@ -13,5 +15,6 @@ int minValue(int a, int b);
 int main() {
     std::cout << "Calculator Initialized\n";
     // each function will be called here once implemented
+    int result=maxValue(int a, int b);
     return 0;
 }
